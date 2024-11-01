4d298a4679302800bf02b171098ec6f08ea96d45
Topskys <2477350343@qq.com>
2024-11-01 19:39:48 +0800

    chore: 配置stylelint检查样式和格式化

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -21,12 +21,12 @@ const title = ref("Hello");
 }
 
 .logo {
-  height: 200rpx;
   width: 200rpx;
+  height: 200rpx;
   margin-top: 200rpx;
-  margin-left: auto;
   margin-right: auto;
   margin-bottom: 50rpx;
+  margin-left: auto;
 }
 
 .text-area {
